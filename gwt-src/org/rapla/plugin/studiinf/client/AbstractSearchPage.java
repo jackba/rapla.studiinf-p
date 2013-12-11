5edae1edb595a345d00defff5a4912d6f1e8c57a
Saskia Brinkmann <saskia.brinkmann@exxeta.de>
2013-12-11 14:26:28 +0100

    added placeholder for keyboard

--- gwt-src/org/rapla/plugin/studiinf/client/AbstractSearchPage.java
+++ gwt-src/org/rapla/plugin/studiinf/client/AbstractSearchPage.java
@@ -2,8 +2,10 @@ package org.rapla.plugin.studiinf.client;
 
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
@@ -14,6 +16,8 @@ public abstract class AbstractSearchPage extends AbstractPage{
 	private Label resultLabel = new Label(i18n.frequentResultsLabel());
 	private Grid results = new Grid(3, 2);
 	private Widget organigramBtn = new Button(i18n.organigram());
+	private AbsolutePanel keyboard = new AbsolutePanel();
+	private HorizontalPanel resultBtns = new HorizontalPanel();
 	private boolean searched = false;
 	
 	
@@ -40,6 +44,16 @@ public AbstractSearchPage(){
 	resultLabel.setStyleName("resultLabel");
 	results.setStyleName("results");
 	organigramBtn.setStyleName("organigramBtn");
+	keyboard.setStyleName("keyboard");
+	resultBtns.setStyleName("resultBtns");
+	
+	resultBtns.add(new Button("Text 1"));
+	resultBtns.add(new Button("Text 2"));
+	resultBtns.add(new Button("Text 3"));
+	resultBtns.add(new Button("Text 4"));
+	resultBtns.add(new Button("Text 5"));
+	resultBtns.add(new Button("Text 6"));
+	
 	searchField.addKeyUpHandler(new KeyUpHandler() {
 		
 	@Override
@@ -53,12 +67,12 @@ public AbstractSearchPage(){
 	}
 	});
 
-	
-	
 	this.add(searchField);
 	this.add(resultLabel);
 	this.add(results);
 	this.add(organigramBtn);
+	this.add(keyboard);
+	this.add(resultBtns);
 }
 
 }
