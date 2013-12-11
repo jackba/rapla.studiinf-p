c3d390ced9fe637a58e8355766d78e0031f6c3e3
Saskia Brinkmann <saskia.brinkmann@exxeta.de>
2013-12-11 14:06:27 +0100

    one box for results and most frequent results

--- gwt-src/org/rapla/plugin/studiinf/client/AbstractSearchPage.java
+++ gwt-src/org/rapla/plugin/studiinf/client/AbstractSearchPage.java
@@ -1,30 +1,63 @@
 package org.rapla.plugin.studiinf.client;
 
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
 
 public abstract class AbstractSearchPage extends AbstractPage{
 
+	private TextBox searchField = new TextBox();
+	private Label resultLabel = new Label(i18n.frequentResultsLabel());
+	private Grid results = new Grid(3, 2);
+	private Widget organigramBtn = new Button(i18n.organigram());
+	private boolean searched = false;
+	
+	
+public boolean isSearched() {
+	return searched;
+}
+
+
+public void setSearched(boolean searched) {
+	this.searched = searched;
+	if (searched == true){
+		resultLabel.setText(i18n.resultLabel());
+	} else {
+		resultLabel.setText(i18n.frequentResultsLabel());
+	}
+	
+}
+
+
 public AbstractSearchPage(){
 	super();
-	Widget searchField = new TextBox();
-	Widget resultLabel = new Label(i18n.resultLabel());
-	Widget frequentLabel = new Label(i18n.frequentResultsLabel());
-	Grid results = new Grid(3, 2);
-	Grid frequent = new Grid(2, 2);
-	Widget organigramBtn = new Button(i18n.organigram());
 	
 	searchField.setStyleName("searchField");
 	resultLabel.setStyleName("resultLabel");
-	frequentLabel.setStyleName("frequentLabel");
 	results.setStyleName("results");
-	frequent.setStyleName("frequent");
 	organigramBtn.setStyleName("organigramBtn");
+	searchField.addKeyUpHandler(new KeyUpHandler() {
+		
+	@Override
+	public void onKeyUp(KeyUpEvent event) {
+		if (searchField.getText().equals("")){
+			setSearched(false);
+		} else {
+			setSearched(true);
+		}
+		
+	}
+	});
+
+	
 	
 	this.add(searchField);
 	this.add(resultLabel);
-	this.add(frequentLabel);
 	this.add(results);
-	this.add(frequent);
 	this.add(organigramBtn);
 }
 
