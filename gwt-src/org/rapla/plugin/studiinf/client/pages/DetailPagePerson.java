a7c57e32bdac2d81271f07e7d3bf71abef330335
Saskia Brinkmann <saskia.brinkmann@exxeta.de>
2013-12-23 17:30:18 +0100

    add infobuttons to detail page person

--- gwt-src/org/rapla/plugin/studiinf/client/pages/DetailPagePerson.java
+++ gwt-src/org/rapla/plugin/studiinf/client/pages/DetailPagePerson.java
@@ -1,9 +1,13 @@
 package org.rapla.plugin.studiinf.client.pages;
 
+import org.rapla.plugin.studiinf.client.ui.IconButton;
 import org.rapla.plugin.studiinf.client.ui.QRBox;
 
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 
 public class DetailPagePerson extends AbstractPage {
 
@@ -12,6 +16,8 @@ public class DetailPagePerson extends AbstractPage {
 	private FlowPanel bottomPanel = new FlowPanel();
 	private FlowPanel middlePanel = new FlowPanel();
 	private Label personInfoLabel = new Label("Information");
+	private Grid infos = new Grid(4, 1);
+	private FlowPanel picture = new FlowPanel();
 	
 	
 	@Override
@@ -23,8 +29,27 @@ public class DetailPagePerson extends AbstractPage {
 		personInfoLabel.setStyleName("personInfoLabel");
 		bottomPanel.setStyleName("personBottomPanel");
 		middlePanel.setStyleName("personMiddlePanel");
+		infos.setStyleName("personInfos");
+		picture.setStyleName("personDetailPicture");
 		
 		personInfoPanel.add(personInfoLabel);
+		personInfoPanel.add(picture);
+		personInfoPanel.add(infos);
+		
+		Image img1 = new Image("img/Kurse.svg");
+		Image img2 = new Image("img/Raum.svg");
+		Image img3 = new Image("img/Kurse.svg");
+		Image img4 = new Image("img/Kurse.svg");
+		
+		Widget roomNrBtn = new IconButton("Raum 1234", img1);
+		Widget mailBtn = new IconButton("test@mail.de", img2);
+		Widget telephoneBtn = new IconButton("0122- 5675765", img3);
+		Widget extraInfosBtn = new IconButton("Zusätzliche Infos", img4);
+		
+		infos.setWidget(0, 0, roomNrBtn);
+		infos.setWidget(1, 0, mailBtn);
+		infos.setWidget(2, 0, telephoneBtn);
+		infos.setWidget(3, 0, extraInfosBtn);
 		
 		this.add(qrBox);
 		this.add(personInfoPanel);
