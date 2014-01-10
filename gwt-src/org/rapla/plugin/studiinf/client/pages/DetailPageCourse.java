5093d534d6b56b659756f7cf0bbfe2039d329dd4
Saskia Brinkmann <saskia.brinkmann@exxeta.de>
2014-01-10 10:16:46 +0100

    details course

--- gwt-src/org/rapla/plugin/studiinf/client/pages/DetailPageCourse.java
+++ gwt-src/org/rapla/plugin/studiinf/client/pages/DetailPageCourse.java
@@ -5,6 +5,7 @@ import org.rapla.plugin.studiinf.client.search.CourseDescriptor;
 import org.rapla.plugin.studiinf.client.ui.IconButton;
 import org.rapla.plugin.studiinf.client.ui.NavigationIconButton;
 
+import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Image;
@@ -13,11 +14,12 @@ import com.google.gwt.user.client.ui.Label;
 public class DetailPageCourse extends AbstractDetailPage {
 	
 	private String courseName;
-	private String studyName = "WI";
-	private String profName  = "PROF";
-	private String roomName  = "23";
+	private String studyName;
+	private String profName;
+	private String roomName;
 	
 	private FlowPanel infoPanel;
+	private FlowPanel bottomPanel;
 	private Label infoLabel;
 	private Grid infos;
 	
@@ -26,12 +28,16 @@ public class DetailPageCourse extends AbstractDetailPage {
 	private NavigationIconButton prof;
 	private NavigationIconButton room;
 	private NavigationIconButton events;
+	private NavigationIconButton prof2;
+	private NavigationIconButton room2;
+	private NavigationIconButton events2;
 	
 	@Override
 	public void init(){
 		super.init();
 		
 		infoPanel = new FlowPanel();
+		bottomPanel = new FlowPanel();
 		infoLabel = new Label("Information");
 		infos = new Grid(5, 1);
 		
@@ -41,7 +47,12 @@ public class DetailPageCourse extends AbstractDetailPage {
 		room = new NavigationIconButton(roomName, new Image(IconProvider.PERSONS), this);
 		events = new NavigationIconButton("Link Rapla", new Image(IconProvider.PERSONS), this);
 		
+		prof2 = new NavigationIconButton(profName, new Image(IconProvider.PERSONS), this);
+		room2 = new NavigationIconButton(roomName, new Image(IconProvider.PERSONS), this);
+		events2 = new NavigationIconButton("Link Rapla", new Image(IconProvider.PERSONS), this);
+		
 		infoPanel.setStyleName("courseInfoPanel");
+		bottomPanel.setStyleName("courseBottomPanel");
 		infoLabel.setStyleName("courseInfoLabel");
 		infos.setStyleName("courseInfos");
 		
@@ -55,9 +66,12 @@ public class DetailPageCourse extends AbstractDetailPage {
 		infoPanel.add(infoLabel);
 		infoPanel.add(infos);
 		
+		bottomPanel.add(prof2);
+		bottomPanel.add(room2);
+		bottomPanel.add(events2);
 		
 		this.add(infoPanel);
-		
+		this.add(bottomPanel);
 	}
 
 	@Override
@@ -94,8 +108,26 @@ public class DetailPageCourse extends AbstractDetailPage {
 
 	@Override
 	protected void handleRessource(String id, ResourceDetail resource) {
-		courseName = "Kurs "+id;
 		CourseDescriptor cd = new CourseDescriptor(resource);
+		
+		courseName = cd.getName();
+		cd.getPicture();
+		
+		cd.getYear();
+		
+		if(!cd.getDepartment().equals("")){
+			studyName = cd.getDepartment();
+			study.getElement().getStyle().setDisplay(Display.INLINE_BLOCK);
+			}else{
+				study.getElement().getStyle().setDisplay(Display.NONE);
+			}
+		if(!cd.getRoomNr().equals("")){
+			roomName = cd.getRoomNr();
+			room.getElement().getStyle().setDisplay(Display.INLINE_BLOCK);
+			}else{
+				room.getElement().getStyle().setDisplay(Display.NONE);
+			}
+		
 		refresh();
 		
 	}
