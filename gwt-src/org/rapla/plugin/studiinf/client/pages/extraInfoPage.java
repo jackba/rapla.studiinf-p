27b07b2cf10caa6b9fcbaf6634a232e21ef85d0e
Saskia Brinkmann <saskia.brinkmann@exxeta.de>
2014-01-15 16:40:33 +0100

    added back button and safeHtml

--- gwt-src/org/rapla/plugin/studiinf/client/pages/extraInfoPage.java
+++ gwt-src/org/rapla/plugin/studiinf/client/pages/extraInfoPage.java
@@ -5,21 +5,32 @@ import java.util.Map;
 
 import org.rapla.plugin.freiraum.common.ResourceDetail;
 import org.rapla.plugin.freiraum.common.ResourceDetailRow;
+import org.rapla.plugin.studiinf.client.Navigation;
 import org.rapla.plugin.studiinf.client.search.PersonDescriptor;
 import org.rapla.plugin.studiinf.client.ui.DetailEntry;
+import org.rapla.plugin.studiinf.client.ui.NavigationButton;
+import org.rapla.plugin.studiinf.client.ui.NavigationIconButton;
 
 import com.google.gwt.thirdparty.guava.common.collect.Table;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlexTable;
 
 public class extraInfoPage extends AbstractDetailPage {
-FlexTable detailsTable;
+private FlexTable detailsTable;
+private NavigationButton backBtn;
+private String id;
+private LinkedList<ResourceDetailRow> details;
 	
 	@Override
 	public void init(){
 		super.init();
 		detailsTable = new FlexTable();
 		detailsTable.setStyleName("detailsTable");
+		
+		backBtn = new NavigationButton("Zurück", Navigation.personDetail, id);
+		backBtn.setStyleName("raplaBackButton");
+		this.add(backBtn); 
+		
 		this.add(detailsTable);
 		
 	}
@@ -46,12 +57,21 @@ FlexTable detailsTable;
 	@Override
 	protected void handleRessource(String id, ResourceDetail resource) {
 		// TODO Auto-generated method stub
+		this.id = id;
 		PersonDescriptor person = new PersonDescriptor(resource);
-		LinkedList<ResourceDetailRow> details = person.getDetails();
+		details = person.getDetails();
+		refresh();
+
+	}
+	
+	@Override
+	public void refresh(){
+		super.refresh();
 		detailsTable.removeAllRows();
 		for (ResourceDetailRow detail:details){
 			addDetails(detail.getLabel(), detail.getValue());
 		}
+		backBtn.setTargetId(id);
 	}
 	
 	public void addDetails(String label, String value){
