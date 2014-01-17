4601eada4263176e35068297633dc0b80e6652f9
Matthias Grabowski <grabbl.matthias@googlemail.com>
2014-01-17 17:29:37 +0100

    Added eventtable

--- gwt-src/org/rapla/plugin/studiinf/client/pages/DetailPageCourse.java
+++ gwt-src/org/rapla/plugin/studiinf/client/pages/DetailPageCourse.java
@@ -1,17 +1,26 @@
 package org.rapla.plugin.studiinf.client.pages;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.rapla.plugin.freiraum.common.Event;
 import org.rapla.plugin.freiraum.common.ResourceDetail;
 import org.rapla.plugin.studiinf.client.IconProvider;
 import org.rapla.plugin.studiinf.client.Navigation;
+import org.rapla.plugin.studiinf.client.ServiceProvider;
 import org.rapla.plugin.studiinf.client.search.CourseDescriptor;
 import org.rapla.plugin.studiinf.client.ui.IconButton;
 import org.rapla.plugin.studiinf.client.ui.NavigationIconButton;
+import org.rapla.plugin.studiinf.client.ui.ResultTable;
 
 import com.google.gwt.dom.client.Style.Display;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwtjsonrpc.common.AsyncCallback;
 
 public class DetailPageCourse extends AbstractDetailPage {
 	
@@ -23,14 +32,18 @@ public class DetailPageCourse extends AbstractDetailPage {
 	
 	private FlowPanel infoPanel;
 	private FlowPanel bottomPanel;
+	private FlowPanel middlePanel;
 	private Label infoLabel;
 	private Grid infos;
+	
+	private List<Event> events;
+	private ResultTable lectures = new ResultTable(new FlowPanel(), 2, 3);
 	
 	private IconButton name;
 	private IconButton study;
 	private NavigationIconButton prof;
 	private NavigationIconButton room;
-	private NavigationIconButton events;
+	private NavigationIconButton raplaButton;
 	private NavigationIconButton prof2;
 	private NavigationIconButton room2;
 	private NavigationIconButton events2;
@@ -40,6 +53,7 @@ public class DetailPageCourse extends AbstractDetailPage {
 		super.init();
 		
 		infoPanel = new FlowPanel();
+		middlePanel = new FlowPanel();
 		bottomPanel = new FlowPanel();
 		infoLabel = new Label("Information");
 		infos = new Grid(5, 1);
@@ -48,7 +62,7 @@ public class DetailPageCourse extends AbstractDetailPage {
 		study = new IconButton(studyName, new Image(IconProvider.COURSES));
 		prof = new NavigationIconButton(profName, new Image(IconProvider.PERSONS), Navigation.personDetail);
 		room = new NavigationIconButton(roomName, new Image(IconProvider.ROOMS), Navigation.roomDetail);
-		events = new NavigationIconButton("Link Rapla", new Image(IconProvider.CALENDAR), Navigation.raplaCourseLink, id);
+		raplaButton = new NavigationIconButton("Link Rapla", new Image(IconProvider.CALENDAR), Navigation.raplaCourseLink, id);
 		
 		prof2 = new NavigationIconButton(profName, new Image(IconProvider.PERSONS), Navigation.personDetail);
 		room2 = new NavigationIconButton(roomName, new Image(IconProvider.ROOMS), Navigation.roomDetail);
@@ -59,6 +73,7 @@ public class DetailPageCourse extends AbstractDetailPage {
 		events2.setStyleName("courseEvents");
 		
 		infoPanel.setStyleName("courseInfoPanel");
+		middlePanel.setStyleName("personMiddlePanel");
 		bottomPanel.setStyleName("courseBottomPanel");
 		infoLabel.setStyleName("courseInfoLabel");
 		infos.setStyleName("courseInfos");
@@ -67,7 +82,7 @@ public class DetailPageCourse extends AbstractDetailPage {
 		infos.setWidget(1, 0, study);
 		infos.setWidget(2, 0, prof);
 		infos.setWidget(3, 0, room);
-		infos.setWidget(4, 0, events);
+		infos.setWidget(4, 0, raplaButton);
 		
 		infoPanel.add(infoLabel);
 		infoPanel.add(infoLabel);
@@ -79,6 +94,7 @@ public class DetailPageCourse extends AbstractDetailPage {
 		
 		this.add(infoPanel);
 		this.add(bottomPanel);
+		this.add(middlePanel);
 	}
 
 	@Override
@@ -104,7 +120,7 @@ public class DetailPageCourse extends AbstractDetailPage {
 		prof.setText(profName);
 		room.setText(roomName);
 		room2.setText(roomName);
-		events.setTargetId(id);
+		raplaButton.setTargetId(id);
 		events2.setTargetId(id);
 	}
 
@@ -141,6 +157,82 @@ public class DetailPageCourse extends AbstractDetailPage {
 			}
 		
 		refresh();
+		
+//		Date dateBegin = new Date();
+//		DateTimeFormat f = DateTimeFormat.getFormat("yyyy-MM-dd HH:mm");
+//		String begin = f.format(dateBegin);
+//		Date dateEnd = new Date();
+//		dateEnd.setHours(23);
+//		dateEnd.setMinutes(59);
+//
+//		String end = f.format(dateEnd);
+//		
+//		Window.alert(id + " " + begin + " " + end);
+//		ServiceProvider.getEvents(begin, end, id, new AsyncCallback<List<Event>>() {
+//
+//			@Override
+//			public void onFailure(Throwable arg0) {
+//				
+//				
+//			}
+//
+//			@Override
+//			public void onSuccess(List<Event> arg0) {
+//				
+//				
+//				events = new ArrayList<Event>(arg0);
+//				
+//				lectures.clear();
+//				Window.alert("onsuccess");
+//				Image lectureRoomImg = new Image(IconProvider.ROOMS);
+//				if(events.size()>=1)
+//				{
+//					Window.alert("vor Label");
+//				Label firstLecture = new Label(events.get(0).toString());
+////				middlePanel.add(firstLecture);
+//				lectures.setWidget(0, 0, firstLecture);
+//				if(!events.get(0).getResources().isEmpty())
+//				{
+//					Window.alert("Vor Raumabfrage");
+//				NavigationIconButton firstLectureRoom = new NavigationIconButton(events.get(0).getResources().get(0).getName(), lectureRoomImg, Navigation.roomDetail, events.get(0).getResources().get(0).getId() );
+//	//			lectureRooms.setWidget(0, 0, firstLectureRoom);
+//				Window.alert("nach Raumabfrage");
+//				lectures.setWidget(0, 1, firstLectureRoom);
+//				}
+//				}
+//				if(events.size()>=2)
+//				{
+//				Label secondLecture = new Label(events.get(1).toString());
+//				
+////				middlePanel.add(secondLecture);
+//				lectures.setWidget(1, 0, secondLecture);
+//				if(!events.get(1).getResources().isEmpty())
+//				{
+//					NavigationIconButton secondLectureRoom = new NavigationIconButton(events.get(1).getResources().get(0).getName(), lectureRoomImg, Navigation.roomDetail, events.get(1).getResources().get(0).getId() );
+//			//	lectureRooms.setWidget(1, 0, secondLectureRoom);
+//					lectures.setWidget(1, 1, secondLectureRoom);
+//			//	lectures.add(secondLecture);
+//				}
+//				}
+//				if(events.size()>=3)
+//				{
+//				Label thirdLecture = new Label(events.get(2).toString());	
+//				
+////				middlePanel.add(thirdLecture);
+//				lectures.setWidget(2, 0, thirdLecture);
+//				if(!events.get(2).getResources().isEmpty())
+//				{
+//					NavigationIconButton thirdLectureRoom = new NavigationIconButton(events.get(2).getResources().get(0).getName(), lectureRoomImg, Navigation.roomDetail, events.get(2).getResources().get(0).getId() );
+//				//lectureRooms.setWidget(2, 0, thirdLectureRoom);
+//					lectures.setWidget(2, 1, thirdLectureRoom);
+//				//lectures.add(thirdLecture);
+//				}
+//				}
+//				middlePanel.add(lectures);			
+//				
+//				}
+//		});
+//		
 		
 	}
 
