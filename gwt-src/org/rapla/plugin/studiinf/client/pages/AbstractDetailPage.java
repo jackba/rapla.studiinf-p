78a1c06ea3d89324196f81b28fb94e89fa098884
Lukas Theis <lukas.theis.wwi11b1@gmail.com>
2014-05-21 11:56:13 +0200

    JavaDoc for AbstractDetailPage

--- gwt-src/org/rapla/plugin/studiinf/client/pages/AbstractDetailPage.java
+++ gwt-src/org/rapla/plugin/studiinf/client/pages/AbstractDetailPage.java
@@ -36,11 +36,17 @@ public abstract class AbstractDetailPage extends AbstractPage {
 	protected List<Event> events;
 	protected ResultTable eventPanel = new ResultTable(new AccessibilityRow(), 2, 3);
 	protected FlowPanel middlePanel;
-
+	/**
+	 * Returns the id of the resource to display
+	 * @return id of the resource
+	 */
 	public String getId() {
 		return id;
 	}
-
+	/**
+	 * Sets the id of the resource to display
+	 * @param id Id of the resource
+	 */
 	public void setId(String id) {
 		this.id = id;
 		if (this.hasDefaultQrBox() == true){
