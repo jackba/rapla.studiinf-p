5dd41907898dea063633db6e2cf7ea61fa39126d
Lukas Theis <lukas.theis.wwi11b1@gmail.com>
2014-06-16 10:44:55 +0200

    Revert "test access for new api"
    This reverts commit 79a41d6739f66ee71ee1a7d03b644b9c7fce4275.

--- gwt-src/org/rapla/plugin/studiinf/client/Studiinf.java
+++ gwt-src/org/rapla/plugin/studiinf/client/Studiinf.java
@@ -36,7 +36,7 @@ public class Studiinf implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final String language = "en";
+		
 		final Button button1 = new Button("test1");
 		final Button button2 = new Button("test2");
 		final Button button3 = new Button("test3");
@@ -124,9 +124,8 @@ public class Studiinf implements EntryPoint {
 
 		button1.addClickHandler(new MyHandler()
 		{
-			
 			void send() {
-					cs.getResources("sonstiges",null,language,new AsyncCallback<List<ResourceDescriptor>>() {
+					cs.getResources("rooms",null,new AsyncCallback<List<ResourceDescriptor>>() {
 						
 						@Override
 						public void onSuccess(List<ResourceDescriptor> test) {
@@ -155,7 +154,7 @@ public class Studiinf implements EntryPoint {
 						serverResponseLabel.setHTML("Failure need to call test 1 first to get id.");
 						return;
 					}
-					cs.getResource(id,language,new AsyncCallback<ResourceDetail>() {
+					cs.getResource(id,new AsyncCallback<ResourceDetail>() {
 						@Override
 						public void onSuccess(ResourceDetail test) {
 							serverResponseLabel.setHTML("Success " + test);
@@ -176,7 +175,7 @@ public class Studiinf implements EntryPoint {
 		button3.addClickHandler(new MyHandler()
 		{
 			void send() {
-					cs.getOrganigram(null,language,new AsyncCallback<List<CategoryDescription>>() {
+					cs.getOrganigram(null,new AsyncCallback<List<CategoryDescription>>() {
 						@Override
 						public void onSuccess(List<CategoryDescription> test) {
 							serverResponseLabel.setHTML("Success " + test);
